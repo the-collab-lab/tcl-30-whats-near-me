399856535a327b60818cb81de5baab3791e3f20e
Megan McKissack <meganmckissack@gmail.com>
2021-07-11 11:19:00 -0700

    updated manifest.json file, added map icon to index.html and app.js files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import map from './map.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={map} className="App-logo" alt="logo" />
+        <p>App To Show You Wikipedia Entry Of What is Nearby</p>
       </header>
     </div>
   );
