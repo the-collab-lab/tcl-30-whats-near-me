89cc0b06e4c1c4997b5b42cf7f2e28f09fd6e5a3
Katia Utochkina <ekaterina.utochkina.2@gmail.com>
2021-07-12 15:46:02 +0300

    Add ‘key’ prop to the defaultProps obj, make GoogleMapReact a self-closing tag

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,12 +3,13 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 
 const Map = () => {
-  const defaultProps = {
+  const { center, zoom, key } = {
     center: {
       lat: 48.8566,
       lng: 2.3522,
     },
     zoom: 11,
+    key: 'AIzaSyCbfV0IAdkkGv-9mmuAkUJNzCPPfGRO6v0',
   };
 
   return (
@@ -16,10 +17,10 @@ const Map = () => {
       <h1>What's Near Me?</h1>
       <div className="google-map">
         <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyCbfV0IAdkkGv-9mmuAkUJNzCPPfGRO6v0' }}
-          defaultCenter={defaultProps.center}
-          defaultZoom={defaultProps.zoom}
-        ></GoogleMapReact>
+          bootstrapURLKeys={{ key }}
+          defaultCenter={center}
+          defaultZoom={zoom}
+        />
       </div>
     </div>
   );
