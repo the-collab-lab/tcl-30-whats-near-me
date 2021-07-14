1dae3ea9755232199c7a92bda81ec99e286baaf3
Megan McKissack <meganmckissack@gmail.com>
2021-07-14 11:53:49 -0700

    resolved merge conflict with main by adding paths and importing components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Map from './components/Map';
+import List from './components/List';
+import Settings from './components/Settings';
+import Help from './components/Help';
+import Nav from './components/Nav';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <Router>
+      <div className="app">
+        <main>
+          <Switch>
+            <Route path="/" exact component={Map} />
+            <Route path="/list" component={List} />
+            <Route path="/settings" component={Settings} />
+            <Route path="/help" component={Help} />
+          </Switch>
+        </main>
+        <Nav />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
