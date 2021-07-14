8a61bedcb58a784f4e30219b6b08dc1941ec91ee
Megan McKissack <meganmckissack@gmail.com>
2021-07-14 11:49:08 -0700

    removed map logo and app title text from app.js sincemain now how the map feature on the front page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import map from './map.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={map} className="App-logo" alt="logo" />
-        <p>App To Show You Wikipedia Entry Of What is Nearby</p>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
