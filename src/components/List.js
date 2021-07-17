dfd89a7200f96d8ce68671933488a9ec9d44ef66
jssckbl <jessicawoudsma@gmail.com>
2021-07-17 18:28:32 -0500

    render pages for center map location

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { usePages } from '../hooks/usePages';
+import Page from './Page';
 
-const List = () => <h1>List</h1>;
+const List = () => {
+  const centerMap = {
+    lat: 48.8566,
+    lng: 2.3522,
+  };
+  const { pages, loading } = usePages(centerMap);
 
+  console.log(pages);
+  return (
+    <div>
+      <h1>List</h1>
+      <div className="pages">
+        {loading && <p>Loading... </p>}
+
+        {pages.map((page) => (
+          <Page key={page.pageid} page={page} />
+        ))}
+      </div>
+    </div>
+  );
+};
 export default List;
