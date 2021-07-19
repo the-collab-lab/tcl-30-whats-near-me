959a8f3b0de92c630a0c6a62868f37e55f4af57d
Alejandro M. Coca <alxmcr@outlook.com>
2021-07-19 13:35:04 -0400

    Display page information

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,16 +1,29 @@
 import React from 'react';
 
 const Page = ({ page }) => {
-  const { pageid, title, thumbnail } = page;
+  const { pageid, title, description, thumbnail, coordinates } = page;
+  const dist = coordinates[0] === undefined ? 0 : coordinates[0].dist;
+
+  console.log('coordinates:', coordinates);
+  console.log('coordinates[0]:', coordinates[0]);
   return (
-    <div>
-      <h1>{title}</h1>
+    <div className="page">
       {thumbnail ? (
-        <img src={thumbnail.source} alt={title} />
+        <img className="page__image" src={thumbnail.source} alt={title} />
       ) : (
-        <p>no image available</p>
+        <i class="fas fa-image page__image--icon"></i>
       )}
-      <p>{pageid}</p>
+      <h2 className="page__title">{title}</h2>
+      <p className="page__description">{description}</p>
+      <p>{dist} Km.</p>
+      <a
+        href={`https://en.wikipedia.org/?curid=${pageid}`}
+        className="page__link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        View on Wikipedia
+      </a>
     </div>
   );
 };
