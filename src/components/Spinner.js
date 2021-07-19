5eefbc56f8ce13d143c113419566c9c7bf09ed2e
Katia Utochkina <ekaterina.utochkina.2@gmail.com>
2021-07-19 10:15:34 +0300

    Add a wrapping div to Spinner component

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const Spinner = () => <div className="spinner" aria-label="loading"></div>;
+const Spinner = () => (
+  <div className="spinner-container">
+    <div className="spinner" aria-label="loading"></div>
+  </div>
+);
 
 export default Spinner;
