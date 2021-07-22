d9462a5880918cfe60ad4cd8fd71d75867b82401
Katia Utochkina <ekaterina.utochkina.2@gmail.com>
2021-07-22 14:03:08 +0300

    Add title, id, aria-label to svg icon to make it accessible

--- src/components/Pin.js
+++ src/components/Pin.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Pin = ({ imageUrl, title }) => (
   <div className="map-icon bounce">
     <img src={imageUrl} alt={title} />
-    <svg viewBox="0 0 32 32">
+    <svg viewBox="0 0 32 32" aria-labelledby="caret-icon">
+      <title id="caret-icon">Caret Icon</title>
       <path fill="currentColor" d="M6,6 26,16 6,26z"></path>
     </svg>
   </div>
