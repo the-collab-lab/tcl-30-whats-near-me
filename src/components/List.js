0cd1e3bb6f20b9c84cca27c33769bde78da087ef
Katia Utochkina <ekaterina.utochkina.2@gmail.com>
2021-07-22 22:30:09 +0300

    [TCL-30] Make nav bar stay always visible in the viewport
    -Add CSS grid to define content areas
    -Add overflow to control which areas should be scrollable and
    non-scrollable

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const List = () => <h1>List</h1>;
+const List = () => (
+  <>
+    <h1>List</h1>
+    <div class="view__content">
+      {/* your <ul> should live inside the view__content div */}
+    </div>
+  </>
+);
 
 export default List;
