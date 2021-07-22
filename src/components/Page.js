16303aa038d359859cd8af41c1c11d4e85f3492d
Alejandro M. Coca <alxmcr@outlook.com>
2021-07-22 17:51:54 -0400

    Change div to li (list item), and improve the distribution of elements.

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,30 +1,37 @@
-import React from 'react';
-
 const Page = ({ page }) => {
   const { pageid, title, description, thumbnail, coordinates } = page;
   const dist = coordinates[0] === undefined ? 0 : coordinates[0].dist;
 
-  console.log('coordinates:', coordinates);
-  console.log('coordinates[0]:', coordinates[0]);
   return (
-    <div className="page">
-      {thumbnail ? (
-        <img className="page__image" src={thumbnail.source} alt={title} />
-      ) : (
-        <i class="fas fa-image page__image--icon"></i>
-      )}
-      <h2 className="page__title">{title}</h2>
-      <p className="page__description">{description}</p>
-      <p>{dist} Km.</p>
-      <a
-        href={`https://en.wikipedia.org/?curid=${pageid}`}
-        className="page__link"
-        target="_blank"
-        rel="noreferrer"
-      >
-        View on Wikipedia
-      </a>
-    </div>
+    <li className="page">
+      <div className="page__info">
+        {thumbnail ? (
+          <figure className="page__cover">
+            <img className="page__image" src={thumbnail.source} alt={title} />
+            <figcaption className="page__figcaption">{dist} Km.</figcaption>
+          </figure>
+        ) : (
+          <div className="page__cover">
+            <i className="fas fa-image page__image--icon"></i>
+            <p className="page__distance">{dist} Km.</p>
+          </div>
+        )}
+        <div className="page__location">
+          <h2 className="page__title">{title}</h2>
+          <p className="page__description">{description}</p>
+        </div>
+        <div className="page__actions">
+          <a
+            href={`https://en.wikipedia.org/?curid=${pageid}`}
+            className="page__link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fas fa-external-link-alt"></i>
+          </a>
+        </div>
+      </div>
+    </li>
   );
 };
 
