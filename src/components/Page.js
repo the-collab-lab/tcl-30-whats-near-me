a434b44fadc3a8cce427093cc416e62efabc5b2b
Alejandro M. Coca <alxmcr@outlook.com>
2021-07-23 12:49:15 -0400

    Improvements with the thumbnail validation

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,21 +1,18 @@
+import defaultLocationImg from '../assets/default-location.svg';
+
 const Page = ({ page }) => {
   const { pageid, title, description, thumbnail, coordinates } = page;
-  const dist = coordinates[0] === undefined ? 0 : coordinates[0].dist;
+  const dist = typeof coordinates[0] === 'undefined' ? 0 : coordinates[0].dist;
+  const imgSrc =
+    typeof thumbnail === 'undefined' ? defaultLocationImg : thumbnail.source;
 
   return (
     <li className="page">
       <div className="page__info">
-        {thumbnail ? (
-          <figure className="page__cover">
-            <img className="page__image" src={thumbnail.source} alt={title} />
-            <figcaption className="page__figcaption">{dist} Km.</figcaption>
-          </figure>
-        ) : (
-          <div className="page__cover">
-            <i className="fas fa-image page__image--icon"></i>
-            <p className="page__distance">{dist} Km.</p>
-          </div>
-        )}
+        <figure className="page__cover">
+          <img className="page__image" src={imgSrc} alt={title} />
+          <figcaption className="page__figcaption">{dist} Km.</figcaption>
+        </figure>
         <div className="page__location">
           <h2 className="page__title">{title}</h2>
           <p className="page__description">{description}</p>
@@ -24,10 +21,8 @@ const Page = ({ page }) => {
           <a
             href={`https://en.wikipedia.org/?curid=${pageid}`}
             className="page__link"
-            target="_blank"
-            rel="noreferrer"
           >
-            <i className="fas fa-external-link-alt"></i>
+            <i className="fas fa-info-circle"></i>
           </a>
         </div>
       </div>
