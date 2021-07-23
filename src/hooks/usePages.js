4f0da8d17075b0e85ad6297838cadb705ca6d49f
Alejandro M. Coca <alxmcr@outlook.com>
2021-07-23 13:40:54 -0400

    - Change centerMap parameter to lat, longitude parameters - Add improvements with our useEffect

--- src/hooks/usePages.js
+++ src/hooks/usePages.js
@@ -1,25 +1,26 @@
 import { useEffect, useState } from 'react';
 import { fixturesnearby } from '../fixtures/fixturesnearby';
+import {
+  getPagesFromResponseAPI,
+  sortPagesByDistance,
+} from '../helpers/helpersAPI';
 
-export const usePages = (centerMap) => {
+export const usePages = (lat = 0, lng = 0) => {
   const [pages, setPages] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     try {
-      const response = fixturesnearby(centerMap);
-      setPages(
-        response.query.pages.sort((a, b) => {
-          return a.coordinates[0].dist - b.coordinates[0].dist;
-        }),
-      );
+      const responseAPI = fixturesnearby({ lat, lng });
+      const pagesResponse = getPagesFromResponseAPI(responseAPI);
+      setPages(sortPagesByDistance(pagesResponse));
     } catch (error) {
       setError(error);
     } finally {
       setLoading(false);
     }
-  }, [centerMap]);
+  }, [lat, lng]);
 
   return { pages, loading, error };
 };
