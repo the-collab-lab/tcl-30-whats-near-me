edf1db8d0b72096d8f434ca703f23cd58f84f0c9
Katia Utochkina <ekaterina.utochkina.2@gmail.com>
2021-07-23 10:22:39 +0300

    Add list items (for testing purposes only)

--- src/components/List.js
+++ src/components/List.js
@@ -4,7 +4,69 @@ const List = () => (
   <>
     <h1>List</h1>
     <div class="view__content">
-      <ul class="list"></ul>
+      <ul class="list">
+        {/* these list items here are for testing purposes only and should be deleted before merging */}
+        <li class="item">
+          <div class="image">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/500px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg"
+              alt="Paris"
+            />
+          </div>
+
+          <div class="text">
+            <h2 class="item__title">Paris</h2>
+            <p class="item__description">
+              Capital and most populous city of France
+            </p>
+          </div>
+        </li>
+        <li class="item">
+          <div class="image">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/500px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg"
+              alt="Paris"
+            />
+          </div>
+
+          <div class="text">
+            <h2 class="item__title">Paris</h2>
+            <p class="item__description">
+              Capital and most populous city of France
+            </p>
+          </div>
+        </li>
+        <li class="item">
+          <div class="image">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/500px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg"
+              alt="Paris"
+            />
+          </div>
+
+          <div class="text">
+            <h2 class="item__title">Paris</h2>
+            <p class="item__description">
+              Capital and most populous city of France
+            </p>
+          </div>
+        </li>
+        <li class="item">
+          <div class="image">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/500px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg"
+              alt="Paris"
+            />
+          </div>
+
+          <div class="text">
+            <h2 class="item__title">Paris</h2>
+            <p class="item__description">
+              Capital and most populous city of France
+            </p>
+          </div>
+        </li>
+      </ul>
     </div>
   </>
 );
