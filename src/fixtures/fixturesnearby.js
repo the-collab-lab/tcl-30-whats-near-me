68ff01c1c3b164baa4f96a8bfe768dbd4177a4b8
Alejandro M. Coca <alxmcr@outlook.com>
2021-07-23 17:19:54 -0400

    Clean up code: data, fixtures, and custom hook

--- src/fixtures/fixturesnearby.js
+++
@@ -1,9 +0,0 @@
-import parisLocationData from '../data/paris.json';
-import zeroLocationData from '../data/zero.json';
-
-export const fixturesnearby = (centerMap) => {
-  if (centerMap.lat === 0 && centerMap.lng === 0) return zeroLocationData;
-  if (centerMap.lat === 48.8566 && centerMap.lng === 2.3522)
-    return parisLocationData;
-  return zeroLocationData;
-};
