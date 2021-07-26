ea3d7b428f28c15bde88eddc20f192302db241ac
Megan McKissack <meganmckissack@gmail.com>
2021-07-26 10:45:21 -0700

    Import MapCenterContext and LocationsContext to List

--- src/components/List.js
+++ src/components/List.js
@@ -1,25 +1,22 @@
 import React, { useContext } from 'react';
-import { SearchResultsContext } from '../context/SearchResults';
-import {
-  getLocationsFromResponseAPI,
-  sortLocationsByDistance,
-} from '../helpers/helpersAPI';
+import { LocationsContext } from '../context/LocationsContext';
+import { sortLocationsByDistance } from '../helpers/helpersAPI';
 import { Location } from './Location';
 
 const List = () => {
-  const responseAPI = useContext(SearchResultsContext);
-  const locationsResponse = getLocationsFromResponseAPI(responseAPI);
-  const locations = sortLocationsByDistance(locationsResponse);
+  const value = useContext(LocationsContext);
+  const { locations } = value;
+  const locationsValidated = sortLocationsByDistance(locations);
 
   return (
     <>
       <h1 className="list__title">List</h1>
       <div className="view__content">
-        {locations?.length === 0 ? (
+        {locationsValidated?.length === 0 ? (
           <p className="list__message">List of locations is empty</p>
         ) : (
           <ul className="list__locations">
-            {locations.map((location) => (
+            {locationsValidated?.map((location) => (
               <Location key={location.pageid} location={location} />
             ))}
           </ul>
