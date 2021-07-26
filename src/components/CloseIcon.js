63004dfb3f77b71e376c6ea5def10301d74b9e7a
Katia Utochkina <ekaterina.utochkina.2@gmail.com>
2021-07-26 11:07:20 +0300

    Add title, id, aria-labelledby to  svg icon to make it accessible

--- src/components/CloseIcon.js
+++ src/components/CloseIcon.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const CloseIcon = () => (
-  <svg viewBox="0 0 32 32">
+  <svg viewBox="0 0 32 32" aria-labelledby="close-icon">
+    <title id="close-icon">Close Icon</title>
     <path d="M32 3.5L28.5 0 16 12.5 3.5 0 0 3.5 12.5 16 0 28.5 3.5 32 16 19.5 28.5 32l3.5-3.5L19.5 16"></path>
   </svg>
 );
