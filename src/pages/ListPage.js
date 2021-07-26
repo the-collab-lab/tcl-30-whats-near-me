732ff72e671f6f8d27f1382cb8dd53a6397594fa
Alejandro M. Coca <alxmcr@outlook.com>
2021-07-26 18:38:21 -0400

    Add Spinner & ErrorMessage components to our pages

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -2,17 +2,23 @@ import React, { useContext } from 'react';
 import { LocationsContext } from '../context/LocationsContext';
 import { sortLocationsByDistance } from '../helpers/helpersAPI';
 import { LocationList } from '../components/LocationList';
+import Spinner from '../components/Spinner';
+import ErrorMessage from '../components/ErrorMessage';
 
 const ListPage = () => {
-  const value = useContext(LocationsContext);
-  const { locations } = value;
+  const valueLocations = useContext(LocationsContext);
+  const { locations, status, error } = valueLocations;
   const locationsSorted = sortLocationsByDistance(locations);
 
   return (
     <>
       <h1 className="list__title">List</h1>
       <div className="view__content">
-        <LocationList locations={locationsSorted} />
+        {status === 'loading' && <Spinner />}
+        {status === 'error' && <ErrorMessage error={error} />}
+        {(status === 'success' || status === 'cancelled') && (
+          <LocationList locations={locationsSorted} />
+        )}
       </div>
     </>
   );
--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -2,23 +2,29 @@ import { useContext } from 'react';
 import { MapCenterContext } from '../context/MapCenterContext';
 import { Map } from '../components/Map';
 import { LocationsContext } from '../context/LocationsContext';
+import Spinner from '../components/Spinner';
+import ErrorMessage from '../components/ErrorMessage';
 
 const MapPage = () => {
   const valueCenterMap = useContext(MapCenterContext);
   const valueLocations = useContext(LocationsContext);
   const { defaultCenterMap, newCenterMap } = valueCenterMap;
-  const { locations } = valueLocations;
+  const { locations, status, error } = valueLocations;
 
   return (
     <>
       <h1>What's Near Me?</h1>
       <div className="view__content">
-        <Map
-          defaultCenterMap={defaultCenterMap}
-          centerMap={newCenterMap}
-          locations={locations}
-          zoom={16}
-        />
+        {status === 'loading' && <Spinner />}
+        {status === 'error' && <ErrorMessage error={error} />}
+        {(status === 'success' || status === 'cancelled') && (
+          <Map
+            defaultCenterMap={defaultCenterMap}
+            centerMap={newCenterMap}
+            locations={locations}
+            zoom={16}
+          />
+        )}
       </div>
     </>
   );
