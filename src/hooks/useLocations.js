c4da69a3b973e9bd663afe5536dce1017f20ef10
Alejandro M. Coca <alxmcr@outlook.com>
2021-07-26 17:55:40 -0400

    Create custom hook useLocations to call to API

---
+++ src/hooks/useLocations.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+
+export const useLocations = (lat, lng) => {
+  const [locations, setLocations] = useState([]);
+  const [status, setStatus] = useState('idle');
+  const [error, setError] = useState(null);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    // Abort fetch
+    let abortController = new AbortController();
+    const { signal } = abortController;
+    signal.addEventListener('abort', () => {
+      // Logs true:
+      console.log(signal.aborted);
+    });
+
+    // Call to API
+    const baseURL = 'https://segdeha.com/api/nearby.php';
+    const completeURL = lat && lng && `${baseURL}?lat=${lat}&lng=${lng}`;
+
+    fetch(completeURL, { signal })
+      .then((response) => {
+        setIsFetching(true);
+        return response.json();
+      })
+      .then((data) => {
+        setLocations(data?.query?.pages);
+        setStatus('success');
+      })
+      .catch((err) => {
+        console.error('Uh oh, an error!', err);
+        setError(err);
+        if (err.name === 'AbortError') {
+          setStatus('Fetch aborted');
+        } else {
+          setStatus(err.message);
+        }
+      })
+      .finally(() => setIsFetching(false));
+
+    // Clean up
+    return function cancel() {
+      abortController?.abort();
+    };
+  }, [lat, lng]);
+
+  return { isFetching, locations, status, error };
+};
