46db69584eafe0f588cf1f10ccb4128786c2db78
Alejandro M. Coca <alxmcr@outlook.com>
2021-07-26 17:37:59 -0400

    Create ListPage page to display a list of locations

---
+++ src/components/ListPage.js
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+import { LocationsContext } from '../context/LocationsContext';
+import { sortLocationsByDistance } from '../helpers/helpersAPI';
+import { LocationList } from './LocationList';
+
+const ListPage = () => {
+  const value = useContext(LocationsContext);
+  const { locations } = value;
+  const locationsSorted = sortLocationsByDistance(locations);
+
+  return (
+    <>
+      <h1 className="list__title">List</h1>
+      <div className="view__content">
+        <LocationList locations={locationsSorted} />
+      </div>
+    </>
+  );
+};
+export default ListPage;
