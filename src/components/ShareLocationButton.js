a0f56351bf41eacbf6ca6245db9e21d5561730b2
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-01 15:15:17 -0400

    Add steps for TCL-30-40, and TCL30-42

--- src/components/ShareLocationButton.js
+++ src/components/ShareLocationButton.js
@@ -15,14 +15,26 @@ const ShareLocationButton = () => {
     const lng = position.coords.longitude;
     const newMapCenter = { lat, lng };
     setNewCenterMap(newMapCenter);
+    // TCL-30-40
+    // 1. call navigator.geolocation.watchPosition()
+    // 2. Update constantly the new center map position
   }
 
   function error() {
+    // TCL30-42
+    // 1. get new  center map (latitude, longitude) from Localstorage
+    // 2. pass new center map (latitude, longitude) to setNewCenterMap
     const defaultCenterMap = {
       lat: 48.8566,
       lng: 2.3522,
     };
     setNewCenterMap(defaultCenterMap);
+
+    // TCL-30-40
+    // 3. Stop watching... navigator.geolocation.clearWatch()
+    // Return to previous state
+    // 4. get new  center map (latitude, longitude) from Localstorage
+    // 5. pass new center map (latitude, longitude) to setNewCenterMap
   }
 
   return <button onClick={getLocation}>Share Location</button>;
