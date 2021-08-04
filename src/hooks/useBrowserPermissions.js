bc00c02da82db2833632295e7a55af9ba9160ab4
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-03 21:32:52 -0400

    Rename the state: statusPermission to permissionStatus

--- src/hooks/useBrowserPermissions.js
+++ src/hooks/useBrowserPermissions.js
@@ -1,15 +1,15 @@
 import { useEffect, useState } from 'react';
 
 export const useBrowserPermissions = (namePermission = 'geolocation') => {
-  const [statusPermission, setStatusPermission] = useState('denied');
+  const [permissionStatus, setPermissionStatus] = useState('denied');
 
   useEffect(() => {
     navigator?.permissions
       ?.query({ name: namePermission })
       .then(function (result) {
-        setStatusPermission(result?.state);
+        setPermissionStatus(result?.state);
       });
   }, []);
 
-  return { statusPermission };
+  return { permissionStatus };
 };
