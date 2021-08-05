1dabeacd5d42c50d4a9bf6a59241d7e3bbfeb406
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-05 14:02:15 -0400

    Extract the current logic to the FlyToMeButton component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,58 +1,30 @@
-import React, { useContext } from 'react';
-
 import { NavLink } from 'react-router-dom';
-import { MapCenterContext } from '../context/MapCenterContext';
+import FlyToMeButton from './FlyToMeButton';
 
-const Nav = () => {
-  const valueCenterMap = useContext(MapCenterContext);
-  const {
-    defaultCenterMap,
-    setUserCenterMap,
-    setUserLocationShared,
-    setNewCenterMap,
-  } = valueCenterMap;
-
-  const handleSuccess = (position) => {
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    const newMapCenter = { lat, lng };
-    setNewCenterMap(newMapCenter);
-    setUserCenterMap(newMapCenter);
-  };
-  const handleError = () => setUserCenterMap(defaultCenterMap);
-
-  const handleFlyToMe = () => {
-    if (navigator?.geolocation) {
-      navigator?.geolocation?.getCurrentPosition(handleSuccess, handleError);
-      setUserLocationShared('yes');
-    }
-  };
-
-  return (
-    <footer>
-      <nav>
-        <ul className="nav">
-          <li>
-            <NavLink exact to="/">
-              Map
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/list">List</NavLink>
-          </li>
-          <li>
-            <NavLink to="/settings">Settings</NavLink>
-          </li>
-          <li>
-            <NavLink to="/help">Help</NavLink>
-          </li>
-          <li>
-            <button onClick={handleFlyToMe}>Fly to me</button>
-          </li>
-        </ul>
-      </nav>
-    </footer>
-  );
-};
+const Nav = () => (
+  <footer>
+    <nav>
+      <ul className="nav">
+        <li>
+          <NavLink exact to="/">
+            Map
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/list">List</NavLink>
+        </li>
+        <li>
+          <NavLink to="/settings">Settings</NavLink>
+        </li>
+        <li>
+          <NavLink to="/help">Help</NavLink>
+        </li>
+        <li>
+          <FlyToMeButton />
+        </li>
+      </ul>
+    </nav>
+  </footer>
+);
 
 export default Nav;
