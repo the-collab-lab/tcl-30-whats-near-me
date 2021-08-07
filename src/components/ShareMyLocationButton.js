aa3f654ee6414badcc1297a530a8402323dab2f0
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-06 22:36:54 -0400

    Create a new button: Share my Location

---
+++ src/components/ShareMyLocationButton.js
@@ -0,0 +1,45 @@
+import { useContext } from 'react';
+import { MapCenterContext } from '../context/MapCenterContext';
+
+const ShareMyLocationButton = ({ disabled = false }) => {
+  const valueCenterMap = useContext(MapCenterContext);
+  const {
+    defaultCenterMap,
+    setUserCenterMap,
+    setNewCenterMap,
+    setTrackingId,
+    userLocationShared,
+    setUserLocationShared,
+  } = valueCenterMap;
+
+  const handleSuccess = (position) => {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const newMapCenter = { lat, lng };
+    setUserCenterMap(newMapCenter);
+
+    if (!userLocationShared) {
+      setNewCenterMap(newMapCenter);
+      setUserLocationShared(true);
+    }
+  };
+  const handleError = () => setUserCenterMap(defaultCenterMap);
+
+  const handleShareLiveLocation = () => {
+    if (navigator?.geolocation) {
+      const newTrackingId = navigator?.geolocation?.watchPosition(
+        handleSuccess,
+        handleError,
+      );
+      setTrackingId(newTrackingId);
+    }
+  };
+
+  return (
+    <button onClick={handleShareLiveLocation} disabled={disabled}>
+      Share My Location
+    </button>
+  );
+};
+
+export default ShareMyLocationButton;
