7100e75ec15ee8c72efb6863772c8d1b58776670
Katia Utochkina <ekaterina.utochkina.2@gmail.com>
2021-08-08 20:23:26 +0300

    Fix invalid html, remove button nesting  inside an <a> tag, remove redundant code

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,42 +1,24 @@
-import { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
-import { MapCenterContext } from '../context/MapCenterContext';
-import FlyToMeButton from './FlyToMeButton';
-import ShareMyLocationButton from './ShareMyLocationButton';
 
-const Nav = () => {
-  const valueCenterMap = useContext(MapCenterContext);
-  const { trackingId, userLocationShared } = valueCenterMap;
-  const wasWatchingLocation = trackingId !== null;
-
-  return (
-    <footer>
-      <nav>
-        <ul className="nav">
-          <li>
-            <NavLink exact to="/">
-              Map
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/list">List</NavLink>
-          </li>
-          <li>
-            <NavLink to="/settings">Settings</NavLink>
-          </li>
-          <li>
-            <NavLink to="/help">Help</NavLink>
-          </li>
-          <li>
-            <ShareMyLocationButton />
-          </li>
-          <li>
-            <FlyToMeButton disabled={!userLocationShared} />
-          </li>
-        </ul>
-      </nav>
-    </footer>
-  );
-};
+const Nav = () => (
+  <nav>
+    <ul className="nav">
+      <li>
+        <NavLink exact to="/">
+          Map
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/list">List</NavLink>
+      </li>
+      <li>
+        <NavLink to="/settings">Settings</NavLink>
+      </li>
+      <li>
+        <NavLink to="/help">Help</NavLink>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Nav;
