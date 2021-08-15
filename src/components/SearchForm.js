2a7d65c8c51f904f66075be77cd21b097d86502d
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-15 15:34:04 -0400

    Call to Places API

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,27 +1,59 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { GoogleMapsContext } from '../context/GoogleMapsContext';
+import { MapCenterContext } from '../context/MapCenterContext';
+import planetSearch from '../assets/search-location-solid.svg';
 
 export const SearchForm = () => {
   const [query, setQuery] = useState('');
+  const valueGoogleMapsAPI = useContext(GoogleMapsContext);
+  const valueMapCenter = useContext(MapCenterContext);
+  const { map, maps } = valueGoogleMapsAPI;
+  const { setNewCenterMap } = valueMapCenter;
+
   const handleQuery = (e) => setQuery(e.target.value);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(`Searching ${query}...`);
+
+    var request = {
+      query,
+      fields: ['name', 'geometry'],
+    };
+
+    var service = new maps.places.PlacesService(map);
+
+    service.findPlaceFromQuery(request, function (results, status) {
+      console.log({ status });
+      if (status === maps.places.PlacesServiceStatus.OK) {
+        // console.log({ results })
+        if (results.length === 0) {
+          alert('No matching results');
+        } else {
+          const firstResult = results[0];
+          setNewCenterMap(firstResult?.geometry?.location);
+        }
+      }
+    });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="query">Place name or address:</label>
-        <input
-          type="search"
-          id="query"
-          name="query"
-          value={query}
-          onChange={handleQuery}
-          placeholder="Enter some Place name or address"
-        />
-      </div>
-      <button type="submit">Search</button>
+    <form onSubmit={handleSubmit} className="search">
+      <label htmlFor="query" className="search__label">
+        Place or address:
+      </label>
+      <input
+        className="search__input"
+        type="search"
+        id="query"
+        name="query"
+        value={query}
+        onChange={handleQuery}
+        placeholder="Search place place or address"
+        maxLength="20"
+      />
+      <button type="submit" className="search__button">
+        <img src={planetSearch} alt="search icon" className="search__icon" />
+      </button>
     </form>
   );
 };
