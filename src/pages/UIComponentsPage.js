e4b781f5f312b9c30068d7fad2f3af30b5a1920e
Alejandro M. Coca <alxmcr@outlook.com>
2021-09-01 02:53:33 -0400

    Add ButtonIcon & ButtonLink components

--- src/pages/UIComponentsPage.js
+++ src/pages/UIComponentsPage.js
@@ -8,10 +8,15 @@ import { ReactComponent as MapIcon } from '../assets/map-icon.svg';
 import { ReactComponent as SearchPlaceIcon } from '../assets/search-place-icon.svg';
 import { ReactComponent as ShareLocationIcon } from '../assets/share-location-icon.svg';
 import { ReactComponent as UserLocationIcon } from '../assets/user-location-icon.svg';
+import { ReactComponent as SettingsIcon } from '../assets/settings-icon.svg';
+import { ReactComponent as HelpIcon } from '../assets/help-icon.svg';
+import { ButtonIcon } from '../components/buttons/ButtonIcon';
+import { ButtonLink } from '../components/buttons/ButtonLink';
+import { NavLink } from 'react-router-dom';
 
 export const UIComponentsPage = () => {
   return (
-    <div>
+    <div className="view__content">
       <h1>UI Components</h1>
       <section>
         <h2>Iconography</h2>
@@ -27,6 +32,46 @@ export const UIComponentsPage = () => {
           <SearchPlaceIcon className="icon" />
           <ShareLocationIcon className="icon" />
           <UserLocationIcon className="icon" />
+          <SettingsIcon className="icon" />
+          <HelpIcon className="icon" />
+        </div>
+      </section>
+      <br />
+      <section>
+        <h2>Buttons (as Buttons)</h2>
+        <br />
+        <div className="icons">
+          <ButtonIcon onClick={() => alert('Share location')}>
+            <ShareLocationIcon className="icon" />
+          </ButtonIcon>
+          <ButtonIcon onClick={() => alert('Fly to me')}>
+            <FlyToMeIcon className="icon" />
+          </ButtonIcon>
+          <ButtonIcon onClick={() => alert('Search place')}>
+            <SearchPlaceIcon className="icon" />
+          </ButtonIcon>
+          <ButtonIcon onClick={() => alert('Close')}>
+            <CloseIcon className="icon" />
+          </ButtonIcon>
+        </div>
+      </section>
+      <br />
+      <section>
+        <h2>NavLinks (as Buttons)</h2>
+        <br />
+        <div className="icons">
+          <ButtonLink href="/" text="Map" className="btn btn--link">
+            <MapIcon className="icon" />
+          </ButtonLink>
+          <ButtonLink href="/list" text="List" className="btn btn--link">
+            <ListIcon className="icon" />
+          </ButtonLink>
+          <ButtonLink href="/settings" className="btn">
+            <SettingsIcon className="icon" />
+          </ButtonLink>
+          <ButtonLink href="/help" className="btn">
+            <HelpIcon className="icon" />
+          </ButtonLink>
         </div>
       </section>
     </div>
