706b844896a6d8322ff50a4e3fb7cda8582e80f4
Jessica Woudsma <38255956+jssckbl@users.noreply.github.com>
2021-09-02 19:56:09 -0500

    update to functioning fly to me button with icon

--- src/components/FlyToMeButton.js
+++ src/components/FlyToMeButton.js
@@ -1,5 +1,7 @@
 import { useContext } from 'react';
 import { MapCenterContext } from '../context/MapCenterContext';
+import { Button } from '../components/Button';
+import { ReactComponent as FlyToMeIcon } from '../assets/fly-to-me-icon.svg';
 
 const FlyToMeButton = ({ disabled = false }) => {
   const valueCenterMap = useContext(MapCenterContext);
@@ -19,9 +21,13 @@ const FlyToMeButton = ({ disabled = false }) => {
   };
 
   return (
-    <button onClick={handleFlyToMe} disabled={disabled}>
-      Fly to me
-    </button>
+    <Button
+      className="btn btn__icon"
+      label="Fly to me"
+      icon={FlyToMeIcon}
+      onClick={handleFlyToMe}
+      disabled={disabled}
+    />
   );
 };
 
